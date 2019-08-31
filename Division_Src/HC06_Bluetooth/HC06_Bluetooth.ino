7ca6dc554ffb7b87603b7e51e18371b30b9f6d79
Noh SiHyun <tlgustlgus0425@naver.com>
2019-09-01 00:56:56 +0900

    Add: blutooth source

--- Division_Src/HC06_Bluetooth/HC06_Bluetooth.ino
+++ Division_Src/HC06_Bluetooth/HC06_Bluetooth.ino
@@ -1,15 +1,27 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial BTSerial(2,3); // software sirial rx, tx
+#define PIN_RX 12
+#define PIN_TX 11
+
+SoftwareSerial btSerial(PIN_TX,PIN_RX); // software sirial rx, tx
 
 byte sensorValue[1024];
 
 void setup() {
   // put your setup code here, to run once:
-  BTSerial.begin(9600);
+  btSerial.begin(9600);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if(btSerial.available())
+  {
+    Serial.write(btSerial.read());
+  }
+  
+  if(Serial.available())
+  {
+    btSerial.write(Serial.read());
+  }
 }
